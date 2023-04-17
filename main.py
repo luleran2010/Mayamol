f3dd689b6cf65a855808024577434a43eff704a4
Leran Lu <luleran2010@hotmail.com>
2023-04-17 13:49:51 +0300

    add function draw*

--- main.py
+++ main.py
@@ -1,57 +1,125 @@
 import numpy as np
-from mayavi import mlab
-from ase.atoms import Atoms, Cell
+from ase.atoms import Atoms
 from ase.io import read
 from ase.data import covalent_radii as radii, atomic_numbers as numbers
 from ase.data.colors import jmol_colors as colors
+from mayavi import mlab
 
-from ase.visualize import view
+from traits.api import HasTraits, Int, Range, Instance, Button, Enum, observe
+from traitsui.api import View, Item, HGroup, ButtonEditor, RangeEditor
+from tvtk.pyface.scene_editor import SceneEditor
+from mayavi.tools.mlab_scene_model import MlabSceneModel
+from mayavi.core.ui.mayavi_scene import MayaviScene
 
-def draw_cell(cell: np.ndarray | Cell, figure=None):
-    lines = []
+ncolors = len(colors)
+element_colormap = np.concatenate((colors*255, np.repeat([[255]], ncolors, axis=0)), axis=1)
+
+def set_element_colormap(glyph):
+    lut = glyph.module_manager.scalar_lut_manager.lut
+    lut.number_of_colors = len(colors)
+    lut.table = element_colormap
+
+def draw(atoms: Atoms, figure=None, mlab=mlab):
+    pos = atoms.positions
+    s = np.array([radii[Z] for Z in atoms.numbers])
+    c = np.array([colors[Z] for Z in atoms.numbers])
+    points = mlab.quiver3d(pos[:,0], pos[:,1], pos[:,2], s, s, s, scalars=atoms.numbers,
+                           mode='sphere', resolution=20, scale_factor=1,
+                           vmax=ncolors-1, vmin=0,
+                           figure=figure)
+    points.glyph.color_mode = 'color_by_scalar'
+    points.glyph.glyph_source.glyph_source.center = [0, 0, 0]
+    set_element_colormap(points)
+
+    # draw the outlines of the cell
+    cell = []
     vertices = np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1], \
-                        [0,1,1], [1,0,1], [1,1,0], [1,1,1]])
+                         [0,1,1], [1,0,1], [1,1,0], [1,1,1]])
     for i in np.arange(vertices.shape[0]):
         for j in np.arange(i+1, vertices.shape[0]):
             if np.linalg.norm(vertices[i,:]-vertices[j,:]) <= 1:
-                line = vertices[[i,j],:] @ cell
-                l = mlab.plot3d(line[:,0], line[:,1], line[:,2])
-                lines.append(l)
-    return lines
+                line = vertices[[i,j],:] @ atoms.cell                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+                l = mlab.plot3d(line[:,0], line[:,1], line[:,2], figure=figure)
+                cell.append(l)
+    mlab.plot3d([1, 2, np.nan, 3, 4], [1, 2, np.nan, 3, 4], [1, 2, np.nan, 3, 4], figure=figure)
+    return points, cell
 
-def draw_atoms(atoms: Atoms, figure) -> dict:
-    points = {}
-    for symbol in set(atoms.symbols):
-        number = numbers[symbol]
-        mask = atoms.symbols == symbol
-        p = mlab.points3d(atoms.positions[mask,0], atoms.positions[mask,1], atoms.positions[mask,2],
-                      scale_factor=radii[number]/radii[1]/2,
-                      color=tuple(colors[number]),
-                      resolution=20,
-                      figure=figure)
-        points[symbol] = p
-    return points
-
-def ashow_single(atoms: Atoms, figure=None):
-    draw_atoms(atoms)
-    draw_cell(atoms.cell, figure)
-
-def ashow_traj(traj: list[Atoms], figure=None):
-    symbols = traj[0].symbols
-    points = draw_atoms(atoms=traj[0], figure=figure)
-    @mlab.animate(delay=50, ui=False)
+def draw_traj(traj: list[Atoms], figure=None, mlab=mlab):
+    pts, cell = draw(traj[0], figure, mlab)
+    @mlab.animate(delay=50)
     def anim():
-        for image in traj:
-            for symbol in points.keys():
-                mask = symbols == symbol
-                points[symbol].mlab_source.trait_set(x=image.positions[mask,0], y=image.positions[mask,1], z=image.positions[mask,2])
+        index = 0
+        while True:
+            index = (index + 1) % len(traj)
+            pos = traj[index].positions
+            pts.mlab_source.trait_set(x=pos[:,0], y=pos[:,1], z = pos[:,2])
             yield
-    anim()
+    a = anim()
 
-# atoms = read('test/c2db-6222.xyz')
-# ashow_single(atoms)
+class TrajectoryAnimator(HasTraits):
+    scene = Instance(MlabSceneModel, ())
+    timestep = Range(1, 200, value = 30)
+    idx_min = Int(0)
+    idx_max = Int()
+    index = Range(low='idx_min', high='idx_max', value=0)
+    play = Button('>')
+    stop = Button('||')
+    goto_first = Button('<<')
+    goto_last = Button('>>')
 
-atoms = read(r'C:\Users\llu22\OneDrive - Oulun yliopisto\Documents\phd\invited\Al2I6\defect\go4\vasprun.xml', index=':')
-print(type(atoms))
-ashow_traj(atoms)
-mlab.show()
+    def __init__(self, traj: list[Atoms]) -> None:
+        super().__init__()
+        self.traj = traj
+        self.pts, self.cell = draw(traj[0], mlab=self.scene.mlab)
+
+        self.idx_max = len(self.traj) - 1
+
+        self.animator = None
+
+    @mlab.animate(delay=30, ui=False)
+    def animate(self):
+        while True:
+            self.index = (self.index + self.timestep) % len(traj)
+            yield
+
+    @observe('index')
+    def on_index_changed(self, event):
+        pos = self.traj[self.index].positions
+        self.pts.mlab_source.trait_set(x=pos[:,0], y=pos[:,1], z = pos[:,2])
+
+    @observe('play')
+    def on_play_clicked(self, event):
+        if self.animator is None:
+            self.animator = self.animate()
+        else:
+            self.animator.timer.Start(50)
+    
+    @observe('stop')
+    def on_stop_clicked(self, event):
+        self.animator.timer.Stop()
+
+    @observe('goto_first')
+    def on_goto_first_clicked(self, event):
+        self.index = 0
+    
+    @observe('goto_last')
+    def on_goto_last_clicked(self, event):
+        self.index = self.idx_max
+
+    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
+                HGroup('timestep', Item('index', editor_args={'mode': 'slider'})),
+                HGroup('goto_first', 'play', 'stop', 'goto_last'),
+                title='Animator'
+                )
+
+def draw_traj_adv(traj: list[Atoms]):
+    animator = TrajectoryAnimator(traj)
+    animator.configure_traits()
+    return animator
+
+if __name__ == '__main__':
+    # mlab.figure(1, bgcolor=(0, 0, 0))
+    traj = read(r'C:\Users\llu22\OneDrive - Oulun yliopisto\Documents\phd\invited\As2I6\defect\md\XDATCAR', index=':')
+    # draw_traj(traj[0::30])
+    # mlab.show()
+    draw_traj_adv(traj)
--- main2.py
+++ main2.py
@@ -1,16 +1,17 @@
 import numpy as np
-
 from mayavi import mlab
-from traits.api import HasTraits, Range, Instance, on_trait_change
-from traitsui.api import View, Item, HGroup, Action, UIInfo, Handler
+from traits.api import HasTraits, Range, Instance, Button, observe
+from traitsui.api import View, Item, HGroup, RangeEditor
 from tvtk.pyface.scene_editor import SceneEditor
 from mayavi.tools.mlab_scene_model import MlabSceneModel
+from pyface.timer.timer import Timer
 from mayavi.core.ui.mayavi_scene import MayaviScene
 
 from ase.atoms import Cell, Atoms
 from ase.io import read
 from ase.data import covalent_radii as radii, atomic_numbers as numbers
 from ase.data.colors import jmol_colors as colors
+
 
 def draw_cell(cell: np.ndarray | Cell, figure=None, mlab=mlab):
     lines = []
@@ -25,52 +26,50 @@ def draw_cell(cell: np.ndarray | Cell, figure=None, mlab=mlab):
     return lines
 
 def draw_atoms(atoms: Atoms, figure=None, mlab=mlab) -> dict:
-    points = {}
-    for symbol in set(atoms.symbols):
-        number = numbers[symbol]
-        mask = atoms.symbols == symbol
-        p = mlab.points3d(atoms.positions[mask,0], atoms.positions[mask,1], atoms.positions[mask,2],
-                      scale_factor=radii[number]/radii[1]/2,
-                      color=tuple(colors[number]),
-                      resolution=50,
-                      figure=figure)
-        points[symbol] = p
+    pos = atoms.positions
+    s = np.array([radii[Z] for Z in atoms.numbers])
+    points = mlab.points3d(pos[:,0], pos[:,1], pos[:,2], s, scale_mode='vector')
+    c = 
     return points
 
-class MayamolHandler(Handler):
-    def play(info: UIInfo):
-        self.mod
-
 class Mayamol(HasTraits):
-    # image = Range(0, 10, 6)
+    step_low = 0
+    step_high = 1
+    step = Range('step_low', 'step_high', value=0)
     scene = Instance(MlabSceneModel, ())
+    play = Button('play')
 
     def __init__(self, traj: list[Atoms] | Atoms) -> None:
         super().__init__()
-        image = Range(0, len(traj)-1, value=0)
-        self.add_trait("image", image)
         self.traj = traj
+        self.step_high = len(self.traj) - 1
         self.mlab = self.scene.mlab
         self.cell = draw_cell(traj[0].cell, mlab=self.mlab)
         self.points = draw_atoms(traj[0], mlab=self.mlab)
 
-    @on_trait_change('image')
-    def update_plot(self):
-        image = self.traj[self.image]
-        for symbol in self.points.keys():
-            mask = image.symbols == symbol
-            self.points[symbol].mlab_source.trait_set(x=image.positions[mask,0],
-                                                      y=image.positions[mask,1],
-                                                      z=image.positions[mask,2])
+    @observe('step')
+    def update_plot(self, event):
+        pos = self.traj[self.step].positions
+        self.points.mlab_source.trait_set(x=pos[:,0], y=pos[:,1], z=pos[:,2])
+            
+    # @observe('play')
+    # def update_plot(self, event):
+    #     def anim():
+    #         for i in range(len(self.traj)):
+    #             self.trait_set(step=i)
+    #             yield
+    #     a = anim()
+    #     t = Timer(50, next(a))
 
-    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
-                HGroup('_',
-                       'image'
-                    ),
-                handler=MayamolHandler(),
-                buttons=[Action(name='Play', action='play')]
-                )
 
 traj = read(r'C:\Users\llu22\OneDrive - Oulun yliopisto\Documents\phd\invited\Al2I6\defect\go4\vasprun.xml', index=':')
 mayamol = Mayamol(traj)
-mayamol.configure_traits()
+
+view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
+            HGroup('_',
+                    'step',
+                    'play'
+                )
+            )
+
+mayamol.configure_traits(view=view)
